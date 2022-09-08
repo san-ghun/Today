8588bc572cd2f1f3f661345ce2bcaedeec41be80
Sanghun <po4865@gmail.com>
2022-09-08 22:39:34 +0200

    feat: Fetch reminders asynchronously
    - Use Swift asynchronous wrapper method that fetches reminders and returns them inline.
    
    TodayError.swift
    - Create a type that can represent common errors that can occur as the app communicate with EventKit. For example, the system might restrict access to reminders on the device.
    - Create a new file named `TodayError.swift`.
    - Create an enumeration named `TodayError` that conforms to `LocalizedError`.
    - Add case and errorDescription.
    
    EKEventStore+AsyncFetch.swift
    - Create an asynchronous wrapper function that can return reminders inline.
    - Create a new file named `EKEventStore+AsyncFetch.swift`.
    	- EKEventStore objects can access a user's calendar events and reminders.
    - Create an async throwing function named `fetchReminders(matching:)`.
    	- Call the generic `withCheckedThrowingContinuation()` Swift function.
    		- Use `continuations` to wrap concurrent callback functions to that the results can be returned inline.
    		- All paths of execution must resume the continuatioin.
    		- Call the EventKit method `fetchReminders(matching:)`.
    			- Write an if-else conditional statement that binds the reminders to a local constant.
    			- Upon success, resume the continuation, returning the reminders.
    			- Upon failure, resume the continuation, throwing the error.

---
+++ Today/Models/TodayError.swift
@@ -0,0 +1,19 @@
+//
+//  TodayError.swift
+//  Today
+//
+//  Created by Sanghun Park on 08.09.22.
+//
+
+import Foundation
+
+enum TodayError: LocalizedError {
+    case failedReadingReminders
+    
+    var errorDescription: String? {
+        switch self {
+        case .failedReadingReminders:
+            return NSLocalizedString("Failed to read reminders.", comment: "failed reading reminders error description")
+        }
+    }
+}
